e33233adaff0fd3c013d85bd9fdfab375b085fee
Mitali Gupta <mitaligupta726@gmail.com>
2024-07-23 22:08:39 -0400

    send data to producer

--- kafka-producer-wiki/src/main/java/net.javaguides.springboot/WikimediaChangesProducer.java
+++ kafka-producer-wiki/src/main/java/net.javaguides.springboot/WikimediaChangesProducer.java
@@ -1,14 +1,23 @@
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+import com.launchdarkly.eventsource.EventHandler;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import com.launchdarkly.eventsource.EventSource;
+
 
 @Service
 public class WikimediaChangesProducer {
 
     private static Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducer.class);
 
-    private kafkaTemplate<String, String> kafkaTemplate;
+    private KafkaTemplate<String, String> kafkaTemplate;
 
-    public WikimediaChangesProducer(kafkaTemplate<String, String> kafkaTemplate) {
+    public WikimediaChangesProducer(KafkaTemplate<String, String> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
@@ -16,7 +25,18 @@ public class WikimediaChangesProducer {
         String topic = "wikimedia_recentchange";
 
         //to read real-time data
-        
+        EventHandler eventHandler= new WikimediaChangesHandler(kafkaTemplate,topic);
+        String url = "";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventsource = builder.build();
+        eventsource.start();
+
+        try {
+            TimeUnit.MINUTES.sleep(10);
+        } catch (InterruptedException e) {
+            LOGGER.error("Thread interrupted during sleep", e);
+            Thread.currentThread().interrupt(); // Restore interrupted status
+        }
     }
     
 }
